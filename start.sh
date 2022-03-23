ec5ace9f367f2d63c228e4dc7c1beffa09f5b891
Jay Sun <jaysunsky@gmail.com>
2022-03-23 11:52:16 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 kill -9 `pgrep tg-bot-linux`
 BIN_DIR=$(cd `dirname $0`; pwd)
-./$BIN_DIR/tg-bot-linux config.json > $BIN_DIR/run-`date +%s`.log 2>&1 &
+$BIN_DIR/tg-bot-linux $BIN_DIR/config.json > $BIN_DIR/run-`date +%s`.log 2>&1 &
 
